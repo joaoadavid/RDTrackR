3842cab14a16d1b5d8c87fbd9e3f92ccf88c157c
joaoadavid <joao.antoniodavid@hotmail.com>
2025-12-01 17:53:29 -0300

    adicionando testes ao backend

---
+++ RDTrackr/backend/RDTrackr-BackEnd/API/tests/UseCases.Test/Contact/SendContactMessagesUseCaseTests.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using RDTrackR.Application.UseCases.Contact;
+using RDTrackR.Communication.Requests.Contact;
+using RDTrackR.Domain.Services.Email;
+using Shouldly;
+
+namespace UseCases.Test.Contact
+{
+    public class SendContactMessageUseCaseTests
+    {
+        [Fact]
+        public async Task Execute_ShouldCallEmailService_WithCorrectParameters()
+        {
+            var mockEmailService = new Mock<IContactEmailService>();
+
+            var useCase = new SendContactMessageUseCase(mockEmailService.Object);
+
+            var request = new RequestContactJson
+            {
+                Name = "João",
+                Email = "joao@example.com",
+                Subject = "Problema no sistema",
+                Message = "Olá, encontrei um erro ao tentar logar."
+            };
+
+            Func<Task> act = async () => await useCase.Execute(request);
+
+            await act.ShouldNotThrowAsync();
+
+            mockEmailService.Verify(x =>
+                x.SendContactMessageAsync(
+                    request.Name,
+                    request.Email,
+                    request.Subject,
+                    request.Message),
+                Times.Once);
+        }
+    }
+}
